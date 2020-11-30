9ac53302975ac8fe8de76b1493f08e0931aa19d1
Ryan Welcher <ryan.welcher@10up.com>
2020-11-30 15:38:36 -0500

    Add breadcrumbs to page.

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,11 +1,23 @@
 import React, { Fragment } from 'react';
 import {useParams} from "react-router-dom";
-import {Link} from "react-router-dom";
+import {Link as RouterLink} from "react-router-dom";
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
 
 import Item from './Item';
 import { itemlist } from './items';
+
+
+const ListHeader =({name}) => (
+	<ListSubheader>
+		<Breadcrumbs>
+		<Link component={RouterLink} to="/">List</Link>
+		<Link component={RouterLink} to={`/category/${name}`}>{name}</Link>
+		</Breadcrumbs>
+	</ListSubheader>
+)
 
 function CategoryList( {set}) {
 	let { name } = useParams();
@@ -14,8 +26,7 @@ function CategoryList( {set}) {
 	const items = sorted.map((name) => <Item key={name} name={name} set={set}/> );
 	return (
 		<Fragment>
-			<Link to="/">Home</Link>
-			<List className="categories" subheader={<ListSubheader>{name}</ListSubheader>}>
+			<List className="categories" subheader={<ListHeader name={name}/>}>
 				{ items }
 			</List>
 		</Fragment>
