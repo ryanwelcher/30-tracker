d1e3db4632f45f8f5381b17009c9d39e0c80b069
Ryan Welcher <ryan.welcher@10up.com>
2020-12-02 14:51:38 -0500

    Remove unused import.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
 	Switch,
 	Route,
 } from "react-router-dom";
-import { useHistory } from 'react-router'    
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Box from '@material-ui/core/Box'
@@ -18,7 +17,6 @@ import AppContext from './AppContext'
 
 function App() {
 	const [count, setCount ] = useState(0);
-	const history = useHistory();
 	useEffect( () => {
 		const saved = localStorage.getItem( 'selectedItems');
 		if ( ! saved ) {
